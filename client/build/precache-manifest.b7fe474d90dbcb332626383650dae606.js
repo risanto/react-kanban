ececb7421916021427a3b2e7324c0f499f7a33d6
risanto <potallegta@gmail.com>
2020-01-05 09:52:18 +0700

    Added build folder

---
+++ client/build/precache-manifest.b7fe474d90dbcb332626383650dae606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ddedde02892cf1a1fd4aa874990c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e5aeb28bc3d49cf8b85",
+    "url": "/static/js/2.eedefa05.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.eedefa05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8c2a15c4acb61663c2e",
+    "url": "/static/js/main.46106544.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
